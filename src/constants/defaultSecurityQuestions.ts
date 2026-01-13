a656d3bf1642276630770fa653291ac2b555f53f
Fahim <ahmed.fahim37@gmail.com>
2026-01-13 17:21:33 +0600

    security q

--- src/constants/defaultSecurityQuestions.ts
+++ src/constants/defaultSecurityQuestions.ts
@@ -1,7 +1,11 @@
 export const defaultSecurityQuestions = [
-  'What is your pet’s name?',
+   'What is your pet’s name?',
   'What is your mother’s maiden name?',
   'What is your favorite color?',
   'What city were you born in?',
   'What was your first school’s name?',
+  'What is your favourite one word food?',
+  'Who is your favourite relation?',
+  'Who is your favourite sports team?',
+  'What is your favourite car model?'
 ]
